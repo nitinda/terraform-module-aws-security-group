efa5e46919eea4cab8c3da80db3b0e31ad20634d
nitin-das <nitin.das@thomson.co.uk>
2019-10-09 19:12:20 +0100

    Modified output

--- security-groups.tf
+++ security-groups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "security_group" {
       from_port   = ingress.value["from_port"]
       to_port     = ingress.value["to_port"]
       protocol    = ingress.value["protocol"]
-      cidr_blocks = ingress.value["cidr_blocks"]
+      cidr_blocks = [ingress.value["cidr_blocks"]]
       description = ingress.value["description"]
     }
   }
