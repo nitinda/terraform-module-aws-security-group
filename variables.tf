7acd1f24796fb80b7521c2b16bc288084d26033e
nitin-das <nitin.das@thomson.co.uk>
2019-11-30 14:19:26 +0000

    Added new code

--- variables.tf
+++ variables.tf
@@ -21,10 +21,10 @@ variable "common_tags" {
 
 variable "ingress_rules" {
     description = "Ingress rules for security group"
-    type        = list(map(string))
+    type        = list(map)
 }
 
 variable "egress_rules" {
     description = "Egress rules for security group"
-    type        = list(map(string))
+    type        = list(map)
 }
