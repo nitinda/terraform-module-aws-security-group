566aa0858fdbc5756636e00f3488fb90c35ade82
nitin-das <nitin.das@thomson.co.uk>
2020-03-17 11:36:37 +0000

    Modified Code

--- variables.tf
+++ variables.tf
@@ -1,34 +1,37 @@
 variable "name_prefix" {
-    description = "Creates a unique name beginning with the specified prefix."
+  description = "Creates a unique name beginning with the specified prefix."
+  default     = null
 }
 
 variable "description" {
-    description = "The security group description. Defaults to Managed by Terraform."
+  description = "The security group description. Defaults to Managed by Terraform"
+  default     = null
 }
 
-variable "revoke_rules_on_delete" {
-  description = "Instruct Terraform to revoke all of the Security Groups attached ingress and egress rules before deleting the rule itself."
-  type        = bool
-  default     = false
-}
-
-variable "vpc_id" {
-    description = "The VPC ID."
-}
-
-variable "tags" {
-    description = "A mapping of tags to assign to the resource."
-    type        = map(string)
-}
-
-variable "ingress_rules" {
+variable "ingress" {
     description = "Ingress rules for security group"
     type        = any
     default     = []
 }
 
-variable "egress_rules" {
+variable "egress" {
     description = "Egress rules for security group"
     type        = any
     default     = []
+}
+
+variable "revoke_rules_on_delete" {
+  description = "Instruct Terraform to revoke all of the Security Groups attached ingress and egress rules before deleting the rule itself"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_id" {
+    description = "The VPC ID"
+}
+
+variable "tags" {
+    description = "A mapping of tags to assign to the resource."
+    type        = map(string)
+    default     = {}
 }
