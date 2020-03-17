17f02f704196368e035be891d68fd4b4a5639155
nitin-das <nitin.das@thomson.co.uk>
2020-03-17 11:34:29 +0000

    Modified Code

--- security-groups.tf
+++ security-groups.tf
@@ -1,41 +1,42 @@
 resource "aws_security_group" "security_group" {
-  name_prefix            = var.name_prefix
-  description            = var.description
+  name        = var.name
+  name_prefix = var.name_prefix
+  description = var.description
+
+  dynamic "ingress" {
+    for_each = var.ingress
+    content {
+      cidr_blocks      = lookup(ingress.value, "cidr_blocks", null)
+      description      = lookup(ingress.value, "description", null)
+      from_port        = lookup(ingress.value, "from_port", null)
+      ipv6_cidr_blocks = lookup(ingress.value, "ipv6_cidr_blocks", null)
+      prefix_list_ids  = lookup(ingress.value, "prefix_list_ids", null)
+      protocol         = lookup(ingress.value, "protocol", null)
+      security_groups  = lookup(ingress.value, "security_groups", null)
+      self             = lookup(ingress.value, "self", null)
+      to_port          = lookup(ingress.value, "to_port", null)
+    }
+  }
+  
+  dynamic "egress" {
+    for_each = var.egress
+    content {
+      cidr_blocks      = lookup(egress.value, "cidr_blocks", null)
+      description      = lookup(egress.value, "description", null)
+      from_port        = lookup(egress.value, "from_port", null)
+      ipv6_cidr_blocks = lookup(egress.value, "ipv6_cidr_blocks", null)
+      prefix_list_ids  = lookup(egress.value, "prefix_list_ids", null)
+      protocol         = lookup(egress.value, "protocol", null)
+      security_groups  = lookup(egress.value, "security_groups", null)
+      self             = lookup(egress.value, "self", null)
+      to_port          = lookup(egress.value, "to_port", null)
+    }
+  }
+  
+  revoke_rules_on_delete = var.revoke_rules_on_delete
   vpc_id                 = var.vpc_id
-  revoke_rules_on_delete = var.revoke_rules_on_delete 
-  tags                   = var.tags
 
   lifecycle {
     create_before_destroy = true
-  }
-
-  dynamic "ingress" {
-      for_each = var.ingress_rules
-      content {
-          from_port        = lookup(ingress.value, "from_port", null)
-          to_port          = lookup(ingress.value, "to_port", null)
-          protocol         = lookup(ingress.value, "protocol", null)
-          cidr_blocks      = lookup(ingress.value, "cidr_blocks", [])
-          description      = lookup(ingress.value, "description", null)
-          self             = lookup(ingress.value, "self", false)
-          security_groups  = lookup(ingress.value, "security_groups", [])
-          ipv6_cidr_blocks = lookup(ingress.value, "ipv6_cidr_blocks", [])
-          prefix_list_ids  = lookup(ingress.value, "prefix_list_ids", [])
-      }
-  }
-
-  dynamic "egress" {
-      for_each = var.egress_rules
-      content {
-          from_port        = lookup(egress.value, "from_port", null)
-          to_port          = lookup(egress.value, "to_port", null)
-          protocol         = lookup(egress.value, "protocol", null)
-          cidr_blocks      = lookup(egress.value, "cidr_blocks", [])
-          description      = lookup(egress.value, "description", null)
-          self             = lookup(egress.value, "self", false)
-          security_groups  = lookup(egress.value, "security_groups", [])
-          ipv6_cidr_blocks = lookup(egress.value, "ipv6_cidr_blocks", [])
-          prefix_list_ids  = lookup(egress.value, "prefix_list_ids", [])
-      }
   }
 }
